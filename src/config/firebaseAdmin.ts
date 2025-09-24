111c3df9368181771dc57f6884dc9a87e4cd77d2
Rich <richadson@ymail.com>
2025-09-24 14:34:46 -0300

    update

--- src/config/firebaseAdmin.ts
+++ src/config/firebaseAdmin.ts
@@ -1,12 +1,10 @@
 import admin from 'firebase-admin';
-require('dotenv').config();
+import 'dotenv/config';
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(process.env.FIREBASE_ADMIN_SDK),
   });
-} else {
-  admin.app();
 }
 
 export default admin;
