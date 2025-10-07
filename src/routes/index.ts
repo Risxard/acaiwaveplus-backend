0638beb9dfede1d46a79edfc23586f052ccc31d8
Rich <richadson@ymail.com>
2025-10-06 21:57:17 -0300

    del: removing users endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,8 @@
 import { Router } from 'express';
-import profileRoutes from './profiles.routes';
-import userRoutes from './user.routes';
 import tmdbRoutes from './tmdb.routes';
 
 const router = Router();
 
-router.use(profileRoutes);
-router.use(userRoutes);
 router.use(tmdbRoutes);
 
 export default router;
